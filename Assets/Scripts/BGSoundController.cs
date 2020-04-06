ec3335b143bd91ec2d98c334186ed6c29f9f5b76
Tasin Ahmed <atasin1998@gmail.com>
2020-04-06 16:52:24 -0400

    Add background music

---
+++ Assets/Scripts/BGSoundController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGSoundController : MonoBehaviour
+{
+    private static BGSoundController instance = null;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public static BGSoundController Instance
+    {
+        get { return instance; }
+    }
+
+    // Update is called once per frame
+    void Awake()
+    {
+        if(instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    void Update()
+    {
+        
+    }
+}
